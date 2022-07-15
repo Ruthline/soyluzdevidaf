ff17e7fb5833ebf0f6db06c47ea8032c792f47e2
Ruthline <rutheveline@hotmail.es>
2022-07-15 16:57:23 -0500

    Text at start page about work us already responsive, added all about this.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,8 @@ import Header from '../../components/header/Header'
 import SliderHome from '../../components/sliderHome/SliderHome'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faWhatsapp} from '@fortawesome/free-brands-svg-icons';
+import TextHomeNT from '../../components/texthomeNT/TextHomeNT';
+
 function Home() {
   return (
     <div>
@@ -11,6 +13,7 @@ function Home() {
 
         <button className='whatsapp'><a href='https://api.whatsapp.com/send?phone=+573193124627&text=Hola!%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20sobre...'><i><FontAwesomeIcon  icon={faWhatsapp}/></i></a></button>
         <SliderHome/>
+        <TextHomeNT/>
         <Footer/>
     </div>
   )
