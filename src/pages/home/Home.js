24ee7d91c8a6bc8f32309497a03b23bf20595547
Ruthline <rutheveline@hotmail.es>
2022-07-18 14:08:28 -0500

    Section about program us, already update with responsive version

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,6 +5,7 @@ import SliderHome from '../../components/sliderHome/SliderHome'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faWhatsapp} from '@fortawesome/free-brands-svg-icons';
 import TextHomeNT from '../../components/texthomeNT/TextHomeNT';
+import HowWeDoIT from '../../components/howWeDoIt/HowWeDoIT';
 
 function Home() {
   return (
@@ -14,6 +15,7 @@ function Home() {
         <button className='whatsapp'><a href='https://api.whatsapp.com/send?phone=+573193124627&text=Hola!%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20sobre...'><i><FontAwesomeIcon  icon={faWhatsapp}/></i></a></button>
         <SliderHome/>
         <TextHomeNT/>
+        <HowWeDoIT/>
         <Footer/>
     </div>
   )
